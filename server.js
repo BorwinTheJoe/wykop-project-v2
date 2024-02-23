77a517102a0a682e035b038a34811071bc82346a
BorwinTheJoe <mateusz.jaltoszuk@gmail.com>
2024-02-23 13:28:37 +0100

    Added Cors Options. Added mongoose Schema for Users. Added a .env File requirement. Added a Mongoose Mongodb Connection.

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
 const cors = require('cors');
-//importing logger method from logEvents.
+const corsOptions = require('./config/corsOptions');
+//importing request logger method from logEvents, and error handler from it's own js file.
 const {reqlogger} = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
 
-// Server port, or a Default port.
+//importing method for connecting to the Database.
+const connectDB = require('./config/dbConn');
+
+// Server port set in .env, or a Default port.
 const PORT = process.env.PORT || 3500;
+
+// Connecting to MongoDB
+connectDB();
 
 // logging requests.
 app.use(reqlogger);
 
 // Cross Origin Resource Sharing.
-app.use(cors());
+app.use(cors(corsOptions));
 
 // middleware for handling url-encoded data
 app.use(express.urlencoded({extended: false}));
@@ -24,9 +32,13 @@ app.use(express.json());
 // middleware for serving static files, like css, from the public folder.
 app.use(express.static(path.join(__dirname, '/public')));
 
+
+
+// --- Routes! --- //
+
 app.use('/', require('./routes/root'));
 
-// Responding with a custom 404 status page.
+// Responding with a custom 404 status page if nothing before caught this.
 app.all('*', (req, res) => {
     res.status(404);
     if (req.accepts('html')) {
