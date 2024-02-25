3039534fd843cd4a8d914ea43fda0be9c5b9e0a2
BorwinTheJoe <mateusz.jaltoszuk@gmail.com>
2024-02-25 14:33:40 +0100

    Created Article model. Also created primitive articleController with some lacks.

---
+++ model/Article.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+// Note: can shorten type to just "name": "Type" from "name": { type: "Type"} where it's the only setting.
+const articleSchema = new Schema({
+    author: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+        required: true
+    },
+    comments: [
+        {
+            body: {
+                type: String
+            },
+            date: {
+                type: Date,
+                default: Date.now
+            }
+        }
+    ],
+    hidden: {
+        type: Boolean,
+    },
+    meta: {
+        votes: {
+            type: Number,
+            default: 0
+        },
+        favs: {
+            type: Number,
+            default: 0
+        }
+    }
+});
+
+module.exports = mongoose.model('Article', articleSchema);
