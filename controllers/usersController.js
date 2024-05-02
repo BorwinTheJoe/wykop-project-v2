d7727c15768e73c2667c5f8041b054bac6078ed9
BorwinTheJoe <mateusz.jaltoszuk@gmail.com>
2024-05-03 00:57:39 +0200

    Edited Comments. - in articleController - in usersController
    Begun working on editUser method.

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -11,6 +11,7 @@ const getAllUsers = async (req, res) => {
 const deleteUser = async (req, res) => {
     if (!req?.body?.id) return res.status(400).json({ message: 'User ID required'});
 
+    // Add ID Filter for IDs which aren't 24 chars and made up of 0-9 a-f
     const user = await User.findOne({ _id: req.body.id}).exec();
     if (!user) {
         return res.status(204).json({ message: `User ID ${req.body.id} not found`});
@@ -29,8 +30,19 @@ const getUser = async (req, res) => {
     res.json(user);
 };
 
+const editUser = async(req, res) => {
+    if (!req?.params?.id) return res.status(400).json({ message: 'User ID required.'});
+
+    const user = await. User.findOne({ _id: req.params.id}).exec();
+    if (!user) {
+        return res.status(204).json({ message: `User ID ${req.params.id} not found.`});
+    }
+    res.json(user);
+}
+
 module.exports = {
     getAllUsers,
     deleteUser,
-    getUser
+    getUser,
+    editUser
 }
